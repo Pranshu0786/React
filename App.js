862ebdecb493e89b2a4b063a077784be31a2af83
PRANSHU SINGH <70695663+Pranshu0786@users.noreply.github.com>
2022-04-24 00:29:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Resturent from "./component/basics/Resturent";
+// import UseState from "./component/Hooks/UseState";
+// import UseEffect from "./component/Hooks/UseEffect";
+// import UseReducer from "./component/Hooks/UseReducer";
+import Todo from "./component/todoreact/Todo";
+const App = () => {
+  return (
+    <div>
+      {/* <Resturent></Resturent> */}
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
